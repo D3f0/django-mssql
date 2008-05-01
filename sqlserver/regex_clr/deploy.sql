724a063a87d09d45bd70d48c2680da536c5dea3d
flangy <devnull@localhost>
2008-05-01 02:46:52 +0000

    Updated REGEX_LIKE deploy script -- still has hardcoded database name and file path.

--- sqlserver/regex_clr/deploy.sql
+++ sqlserver/regex_clr/deploy.sql
@@ -1,3 +1,7 @@
+-- Use the proper database
+use [django_test_backend]
+GO
+
 -- Enable CLR in this database
 sp_configure 'show advanced options', 1;
 GO
@@ -9,7 +13,8 @@ RECONFIGURE;
 GO
 
 -- Create the assembly in the database from the path can this be done via a file path?
--- CREATE ASSEMBLY regex_clr from 'c:\Projects\django-mssql\sqlserver\regex_clr\bin\Debug\regex_clr.dll' WITH PERMISSION_SET = SAFE
+CREATE ASSEMBLY regex_clr from 'C:\Projects\django-mssql\sqlserver\regex_clr\bin\Debug\regex_clr.dll' WITH PERMISSION_SET = SAFE
+GO
 
 -- Pull in the User Defined Function from the assembly
 create function REGEXP_LIKE
@@ -20,3 +25,4 @@ create function REGEXP_LIKE
 ) 
 RETURNS INT  AS 
 EXTERNAL NAME regex_clr.UserDefinedFunctions.REGEXP_LIKE
+GO
