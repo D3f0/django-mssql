025f44451c30a7692dae6905816e0bf332303736
mmanfre <devnull@localhost>
2010-10-27 16:05:03 -0400

    Added setup.py to allow installing as a package.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+version = __import__('sqlserver_ado').get_version()
+
+setup(
+    name='django-mssql',
+    version=version,
+    maintainer='Michael Manfre',
+    maintainer_email='mmanfre@gmail.com',
+    url='http://django-mssql.googlecode.com/',
+    description="Django backend database support for MS SQL 2005 and up.",
+    license='Apache License, Version 2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+)
